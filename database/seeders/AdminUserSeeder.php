27d3a39ab7fe0b8d3ed8662c59beca4cf9cb7eb0
JoanOsorio <joan_osorio28@outlook.com>
2023-08-10 18:58:54 -0500

    Cambio en el seeder

--- database/seeders/AdminUserSeeder.php
+++ database/seeders/AdminUserSeeder.php
@@ -6,11 +6,13 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Events\UserActionEvent;
 
 class AdminUserSeeder extends Seeder
 {
     public function run()
     {
+       
         // Crear el usuario administrador
         User::create([
             'name' => 'Administrador',
@@ -18,5 +20,11 @@ class AdminUserSeeder extends Seeder
             'password' => Hash::make('password'), // Aquí define la contraseña del administrador
             'role' => 1
         ]);
+
+        $user = 1;
+        $actionDetails = 'Acción realizada';
+
+        // Crea y envía una instancia de UserActionEvent con los argumentos adecuados
+        event(new UserActionEvent($user, $actionDetails));
     }
 }
